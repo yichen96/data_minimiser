d7645e34d8720b87544a191bb5774ca08b8a02cf
Yichen Liu <liuyichenpanda@gmail.com>
2016-12-18 10:54:34 +0000

    creating plot for final use

--- final_task4_4.py
+++ final_task4_4.py
@@ -30,32 +30,39 @@ min_NLL = pb.nll2d(min_tau, min_signal_fraction)
 tau_l = np.arange(0.4, 0.42, 0.001)
 signalfrac_l = np.arange(0.97, 0.993, 0.001)
 
-error_tau_array = np.array([[[min_tau-error_tau_n, min_tau+error_tau_p], [min_signal_fraction, min_signal_fraction]]])
-error_sigfrac_array = np.array([[[min_tau, min_tau],
+error_tau_coord = np.array([[[min_tau - error_tau_n, min_tau + error_tau_p], [min_signal_fraction, min_signal_fraction]]])
+error_sigfrac_coord = np.array([[[min_tau, min_tau],
                                  [min_signal_fraction-error_sigfrac_n, min_signal_fraction+error_sigfrac_p]]])
 
+
+plt.figure(1)
 Y, X = np.meshgrid(signalfrac_l, tau_l)
 Z = np.ndarray(shape=(tau_l.size, signalfrac_l.size))
 for i in range(tau_l.size):
     for j in range(signalfrac_l.size):
         Z[i][j] = pb.nll2d(tau_l[i], signalfrac_l[j])
-levels = np.arange(min_NLL, 6218.9, 0.5)
-CS = plt.contourf(X, Y, Z, levels=levels, alpha=0.5, colors='g')
-plt.clabel(CS, inline=1, fmt='%.1f', fontsize=13)
-green_patch = mpatches.Patch(color='green', alpha=0.5, label=r'1 $\sigma$ from the minimum')
-black_patch = mpatches.Patch(facecolor='white', edgecolor='black', label=r'1 $\sigma$ delimited by error found from $\tau$ and signal fraction')
-minimum, = plt.plot(min_tau, min_signal_fraction, 'ro', label='minimum where NLL = %.1f' % min_NLL)
-plt.annotate('minimum at (%.3f, %.3f)' % (min_tau, min_signal_fraction), xy=(0.407, 0.985))
+levels = np.arange(min_NLL, 6218.94, 0.5)
+CS = plt.contourf(X, Y, Z, levels=levels, alpha=0.3, colors='g')
 
-plt.hlines(error_sigfrac_array[0][1][0], error_tau_array[0][0][0], error_tau_array[0][0][1])
-plt.hlines(error_sigfrac_array[0][1][1], error_tau_array[0][0][0], error_tau_array[0][0][1])
-plt.vlines(error_tau_array[0][0][0], error_sigfrac_array[0][1][0], error_sigfrac_array[0][1][1])
-plt.vlines(error_tau_array[0][0][1], error_sigfrac_array[0][1][0], error_sigfrac_array[0][1][1])
+green_patch = mpatches.Patch(color='green', alpha=0.3, label=r'$1 \sigma$ area determined from NLL')
+black_patch = mpatches.Patch(facecolor='white', edgecolor='black', label=r'$1 \sigma$ area delimited by error bars')
+
+minimum, = plt.plot(min_tau, min_signal_fraction, 'ro', label='minimum where NLL = %.1f' % min_NLL)
+plt.annotate('minimum at (%.3f, %.3f)' % (min_tau, min_signal_fraction), xy=(0.4052, 0.984), fontsize=13)
+
+asymmetric_error_tau = np.array([np.array([error_tau_n]), np.array([error_tau_p])])
+asymmetric_error_sigfrac = np.array([np.array([error_sigfrac_n]), np.array([error_sigfrac_p])])
+
+plt.errorbar(min_tau, min_signal_fraction, xerr=asymmetric_error_tau, yerr=asymmetric_error_sigfrac, ecolor='red')
+plt.hlines(error_sigfrac_coord[0][1][0], error_tau_coord[0][0][0], error_tau_coord[0][0][1])
+plt.hlines(error_sigfrac_coord[0][1][1], error_tau_coord[0][0][0], error_tau_coord[0][0][1])
+plt.vlines(error_tau_coord[0][0][0], error_sigfrac_coord[0][1][0], error_sigfrac_coord[0][1][1])
+plt.vlines(error_tau_coord[0][0][1], error_sigfrac_coord[0][1][0], error_sigfrac_coord[0][1][1])
 
 plt.xlim(0.4, 0.42)
 plt.ylim(0.97, 1)
 plt.xlabel(r'$\tau$ (picosecond)', fontsize=16)
 plt.ylabel('signal fraction', fontsize=16)
-plt.legend(numpoints=1, fontsize=13, handles=[minimum, green_patch, black_patch])
+plt.legend(numpoints=1, fontsize=15, handles=[minimum, green_patch, black_patch])
 plt.title('Standard Deviation of the Average Lifetime and Signal Fraction')
 plt.show()
